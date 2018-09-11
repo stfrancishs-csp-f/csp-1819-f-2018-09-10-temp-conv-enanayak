034e4208963ffc1152e1df3dcdb08b486a974027
Ena Nayak <20nayak@stfrancishs.org>
2018-09-11 17:37:42 +0000

    init

--- dowhile.c
+++ dowhile.c
@@ -4,8 +4,13 @@ int main(void)
 {
     int fahr;
 
-    for (fahr = -50; fahr <= 120; fahr = fahr +5)
+    for (fahr = -100; fahr <= 120; fahr = fahr +20)
+
+    //fahr values from -100 to 120 in incriments of 10
     {
-        printf ("d - %3d c - %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+        printf ("d - %4d c - %6.2f\n", fahr, (5.0/9.0)*(fahr-32));
+
+        //4 spaces (left), 6 spaces and 2 decimal places (right), fahr value multiplied by 5.0/9.0 subtracted by 32
+
     }
 }
